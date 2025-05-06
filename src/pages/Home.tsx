a1fc87a26d9c25dc5c6b0ad9746b26974b1de9d5
Hasan Chinthaka <pgrhasanchinthaka@gmail.com>
2025-05-06 20:52:46 +0530

    add color pallet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='justify-center items-center flex flex-col h-screen text-4xl font-bold text-secondary'>
+    <div className='justify-center items-center flex flex-col h-screen text-4xl font-bold text-primary bg-primary'>
       <p>zoftmo Solution (pvt) Ltd.</p>
       <p>zoftmo.com</p>
     </div>
