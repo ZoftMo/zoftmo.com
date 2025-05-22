d5c771b11c3b523c81620a4e72ea53ea79fb74d1
Thisandu Edirisinghe <thisandu.n.edirisinghe@gmail.com>
2025-05-22 17:17:06 +0530

    costomize the home page✅

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,24 @@ import React from 'react'
 const Home = () => {
   return (
     <>
+  <div className="bg-primary relative h-screen px-4 text-black md:text-4xl">
+  <div className="absolute left-1/2 top-[120px] -translate-x-1/2 transform text-center">
+    <p>
+      Building <span className="text-blue-300">Smart Solutions</span> for a <br className="hidden md:block" />
+    </p>
+    <p>Smarter World</p>
+  </div>
+</div>
+
+
+
+
     
-    <div className='justify-center items-center flex flex-col h-screen md:text-4xl font-bold text-primary bg-primary'>
-      <p>zoftmo Solution (pvt) Ltd.</p>
-      <p>zoftmo.com</p>
-    </div>
+
+
+
+
+    
     </>
   )
 }
